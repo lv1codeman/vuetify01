082786888ec520355fa8d5421b3cae96b70f20ec
lv1codeman <lv1codeman@gmail.com>
2024-04-18 14:40:10 +0800

    login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -11,24 +11,34 @@ function submit() {
 </script>
 
 <template>
-  <v-sheet class="mx-auto" width="300">
-    <v-form @submit.prevent="submit">
-      <v-text-field
-        v-model="form.email"
-        clearable
-        label="Email"
-        prepend-inner-icon="mdi-mail"
-        variant="solo"
-      ></v-text-field>
-      <v-text-field
-        v-model="form.password"
-        clearable
-        label="Password"
-        type="password"
-        prepend-inner-icon="mdi-key"
-        variant="solo"
-      ></v-text-field>
-      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-    </v-form>
-  </v-sheet>
+  <v-card class="pa-4 mx-auto" width="300">
+    <v-card-title class="text-center"> Login Here </v-card-title>
+    <v-card-item>
+      <v-form @submit.prevent="submit">
+        <v-text-field
+          v-model="form.email"
+          clearable
+          label="Email"
+          prepend-inner-icon="mdi-mail"
+          variant="solo"
+        ></v-text-field>
+        <v-text-field
+          v-model="form.password"
+          clearable
+          label="Password"
+          type="password"
+          prepend-inner-icon="mdi-key"
+          variant="solo"
+        ></v-text-field>
+        <v-btn class="mt-2" color="red-darken-1" type="submit" block
+          >Submit</v-btn
+        >
+      </v-form>
+    </v-card-item>
+    <v-card-action>
+      <div class="mx-4">
+        <v-btn block to="/register">Register</v-btn>
+      </div>
+    </v-card-action>
+  </v-card>
 </template>
